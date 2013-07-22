242779c3057352706fa3e4d3413dba6f23611f0a
Philip Senechal <philip.senechal@gmail.com>
2013-07-21 21:54:03 -0700

    Initial commit scripts

---
+++ Scripts/custom.js
@@ -0,0 +1,29 @@
+﻿// sidebar arrow injection
+jQuery.fn.tablerize = function () {
+    return this.each(function () {
+        var table = $('<table>');
+        var tbody = $('<tbody>');
+        $(this).find('li').each(function (i) {
+            var values = $(this).html().split('*');
+            if (i == 0) {
+                var thead = $('<thead>');
+                var tr = $('<tr>');
+                $.each(values, function (y) {
+                    tr.append($('<th>').html(values[y]));
+                });
+                table.append(thead.append(tr));
+            } else {
+                var tr = $('<tr>');
+                $.each(values, function (y) {
+                    tr.append($('<td>').html(values[y]));
+                });
+                tbody.append(tr);
+            }
+        });
+        $(this).after(table.append(tbody)).remove();
+    });
+};
+
+$(function () {
+    $('#dashboard-menu > li.active > a').append('<div class="pointer"><div class="arrow"></div><div class="arrow_border"></div></div>');
+});
